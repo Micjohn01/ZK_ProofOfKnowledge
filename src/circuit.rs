4c419ccb8de323fceffcba13000ae25372d90846
Micjohn01 <michaelolamilekanjohn0@gmail.com>
2025-02-11 23:26:21 +0100

    circuit updated

--- src/circuit.rs
+++ src/circuit.rs
@@ -49,21 +49,6 @@ fn add_layer(&mut self, operations: Vec<(Operation, Vec<GateId>)>) {
     self.layers.push(Layer { gates: new_layer});
 }
 
-// fn evaluate(&mut self) {
-//     for layer_idx in 1..self.layers.len() {
-//         let prev_layer = &self.layers[layer_idx - 1].gates;
-//         let current_layer  = &mut self.layers[layer_idx].gates;
-
-//         for gate in current_layer.iter_mut() {
-//             let inputs: Vec<u64> = gate.inputs.iter().map(|id| prev_layer[id.0].value.unwrap()).collect();
-
-//             gate.value = Some(match gate.operator{
-//                 Operation::Add => inputs.iter().sum(),
-//                 Operation::Mul => inputs.iter().product(),
-//             });
-//         }
-//     }
-// }
 
 fn evaluate(&mut self) {
     for layer_idx in 1..self.layers.len() {
